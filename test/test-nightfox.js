bb0e5e3efbe296a7b0de5a19d79b210c22e644f0
Martino Visintin <mvisinti@thoughtworks.com>
2015-02-18 11:23:51 -0500

    implement icon path generation

--- test/test-nightfox.js
+++ test/test-nightfox.js
@@ -1,8 +1,15 @@
-var nightfox = require('./nightfox.js');
+var nfx = require('./nightfox.js');
 
 exports['test new_theme_from'] = function(assert) {
-  assert.ok(nightfox.new_theme_from('light') == 'dark');
-  assert.ok(nightfox.new_theme_from('dark') == 'light');
+  assert.ok(nfx.new_theme_from('light') == 'dark');
+  assert.ok(nfx.new_theme_from('dark') == 'light');
+};
+
+exports['test icon_for'] = function(assert) {
+  assert.ok(nfx.icon_for('light')['32'] == './from_light.svg');
+  assert.ok(nfx.icon_for('dark')['32'] == './from_dark.svg');
+  assert.ok(nfx.icon_for('dark')['16'] == './from_dark.svg');
+  assert.ok(nfx.icon_for()['32'] == './from_dark.svg', 'defaults to dark');
 };
 
 require("sdk/test").run(exports);
